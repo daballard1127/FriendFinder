2bc32a8b73769e4c98acd440fb6a25b991dd7321
daballard1127 <denise_ballard@att.net>
2017-12-22 00:45:50 -0600

    Last set of troubleshoot display

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -28,48 +28,48 @@ router.post("/api/friends", function(req, res) {
 		console.log('userResponses = ' , userResponses);
 
 
-		// This is where the wheels fall off the bus. It cannot read the array of data
-		// userResponses comes back undefined. I can not find the issue
-
-		// The following is the rest of the pseudo code to compare and find the Best Match
+		// Take the user input scores and for each person in the friends array
+		// compute the absolute value difference value in the arrays.
+		// Take each calucated value and store it in it's own array
 
 	
 		// Compute best friend match
-	// 	var matchName = '';
-	// 	var matchImage = '';
-	// 	var totalDifference = 10000; // Make the initial value big for comparison
+		var matchName = '';
+		var matchImage = '';
+		var totalDifference = 100; // Make the initial value big for comparison
 
 	// 	// Examine all existing friends in the list
-	// 	for (var i = 0; i < friends.length; i++) {
-	// 		console.log('friend = ' + JSON.stringify(friends[i]));
+		for (var i = 0; i < friends.length; i++) {
+			console.log('friend = ' + JSON.stringify(friends[i]));
 
-	// 		// Compute differenes for each question
-	// 		var diff = 0;
-	// 		for (var j = 0; j < userResponses.length; j++) {
-	// 			diff += Math.abs(friends[i].scores[j] - userResponses[j]);
-	// 		}
-	// 		// console.log('diff = ' + diff);
+			// Compute differenes for each question
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+			}
+			console.log('diff = ' + diff);
 
 	// 		// If lowest difference, record the friend match
-	// 		if (diff < totalDifference) {
-	// 			// console.log('Closest match found = ' + diff);
-	// 			// console.log('Friend name = ' + friends[i].name);
-	// 			// console.log('Friend image = ' + friends[i].photo);
+			if (diff < totalDifference) {
+				console.log('Closest match found = ' + diff);
+				console.log('Friend name = ' + friends[i].name);
+				console.log('Friend image = ' + friends[i].photo);
 
-	// 			totalDifference = diff;
-	// 			matchName = friends[i].name;
-	// 			matchImage = friends[i].photo;
-	// 		}
-	// 	}
+				totalDifference = diff;
+				matchName = friends[i].name;
+				matchImage = friends[i].photo;
+			}
+		}
 
 	// 	// Add new user
-	// 	friends.push(userInput);
+		friends.push(userInput);
 
-	// 	// Send appropriate response
-	// 	res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
-	// });
+		
+		res.send({ matchName: matchName,
+				   matchImage: matchImage});
+	});
 	
 
- });
+ 
 
 module.exports = router;
