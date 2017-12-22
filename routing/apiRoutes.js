aa00c16a2b1ec298198674522b81e0f5b8963578
daballard1127 <denise_ballard@att.net>
2017-12-21 21:46:16 -0600

    Troubleshooting homework

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -11,6 +11,7 @@ router.get("/api/friends", function(req, res) {
 
 	//  This will be used to display a JSON of all possible friends.
 	res.json(friends);
+	// console.log("get", friends)
 
 });
 
@@ -20,10 +21,11 @@ router.post("/api/friends", function(req, res) {
 
 	// Capture the user input object
 		var userInput = req.body;
-		console.log('userInput = ' + JSON.stringify(req.body));
-
+		console.log('userInput = ' + JSON.stringify(userInput));
+		var userName = userInput.name;
+		console.log("userName = " + userName);
 		var userResponses = userInput.scores;
-		console.log('userResponses = ' + userResponses);
+		console.log('userResponses = ' , userResponses);
 
 
 		// This is where the wheels fall off the bus. It cannot read the array of data
@@ -33,41 +35,41 @@ router.post("/api/friends", function(req, res) {
 
 	
 		// Compute best friend match
-		var matchName = '';
-		var matchImage = '';
-		var totalDifference = 10000; // Make the initial value big for comparison
+	// 	var matchName = '';
+	// 	var matchImage = '';
+	// 	var totalDifference = 10000; // Make the initial value big for comparison
 
-		// Examine all existing friends in the list
-		for (var i = 0; i < friends.length; i++) {
-			console.log('friend = ' + JSON.stringify(friends[i]));
+	// 	// Examine all existing friends in the list
+	// 	for (var i = 0; i < friends.length; i++) {
+	// 		console.log('friend = ' + JSON.stringify(friends[i]));
 
-			// Compute differenes for each question
-			var diff = 0;
-			for (var j = 0; j < userResponses.length; j++) {
-				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
-			}
-			// console.log('diff = ' + diff);
+	// 		// Compute differenes for each question
+	// 		var diff = 0;
+	// 		for (var j = 0; j < userResponses.length; j++) {
+	// 			diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+	// 		}
+	// 		// console.log('diff = ' + diff);
 
-			// If lowest difference, record the friend match
-			if (diff < totalDifference) {
-				// console.log('Closest match found = ' + diff);
-				// console.log('Friend name = ' + friends[i].name);
-				// console.log('Friend image = ' + friends[i].photo);
+	// 		// If lowest difference, record the friend match
+	// 		if (diff < totalDifference) {
+	// 			// console.log('Closest match found = ' + diff);
+	// 			// console.log('Friend name = ' + friends[i].name);
+	// 			// console.log('Friend image = ' + friends[i].photo);
 
-				totalDifference = diff;
-				matchName = friends[i].name;
-				matchImage = friends[i].photo;
-			}
-		}
+	// 			totalDifference = diff;
+	// 			matchName = friends[i].name;
+	// 			matchImage = friends[i].photo;
+	// 		}
+	// 	}
 
-		// Add new user
-		friends.push(userInput);
+	// 	// Add new user
+	// 	friends.push(userInput);
 
-		// Send appropriate response
-		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
-	});
+	// 	// Send appropriate response
+	// 	res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+	// });
 	
 
-// });
+ });
 
 module.exports = router;
